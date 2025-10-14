cd16e0cf338b9526b64c7334bf49553eb8f40767
Nate Sinnott <ns@nathanielsinnott.com>
2025-10-14 04:35:55 +0100

    Initial Commit

---
+++ OHPOS/OHPOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  OHPOSApp.swift
+//  OHPOS
+//
+//  Created by Nate Sinnott on 14/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct OHPOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
